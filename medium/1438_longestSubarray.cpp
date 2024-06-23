2e5b5b9cc62732652267a805c34cffdfa0d0d9b6
Kutay07 <kutaycan007@gmail.com>
2024-06-23 21:39:51 +0300

    longestSubarray

--- medium/1438_longestSubarray.cpp
+++ medium/1438_longestSubarray.cpp
@@ -40,6 +40,7 @@ Medium
 
 Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
 
+
 Example 1:
 Input: nums = [8,2,4,7], limit = 4
 Output: 2
